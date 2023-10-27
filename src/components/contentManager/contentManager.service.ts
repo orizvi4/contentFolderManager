1db0812f483c4bcba4e8365c20461904d54b62b1
orizvi4 <orizvi4@gmail.com>
2023-10-27 23:02:28 +0300

    added logs

--- src/components/contentManager/contentManager.service.ts
+++ src/components/contentManager/contentManager.service.ts
@@ -4,6 +4,21 @@ import * as fs from 'fs';
 import { Recording } from "./models/recording.model";
 import { Model } from "mongoose";
 
+const bunyan = require('bunyan');
+const logstashStream = require('bunyan-logstash-tcp').createStream({
+    host: '127.0.0.1',
+    port: 5000
+});
+
+const logger = bunyan.createLogger({
+    elasticIndex: 'content-folder-manager',
+    name: 'content-folder-manager',
+    category: 'code',
+    streams: [{
+        stream: logstashStream
+    }],
+});
+
 const CONTENT_PATH = 'C:/Program Files/Wowza Media Systems/Wowza Streaming Engine 4.8.24+4/content';
 @Injectable()
 export class ContentManagerService {
@@ -16,18 +31,19 @@ export class ContentManagerService {
                     if (err) {
                         return reject(err);
                     }
-                    resolve('File deleted successfully');
+                    resolve(true);
                 });
             })
             const res = await this.recordingModel.deleteOne({ url: `${CONTENT_PATH}/${file}` });
             if (res == null) {
+                logger.error({category: 'mongoDB'}, "didn't delete the recording: " + file);
                 return false;
             }
-            console.log("deleted succesfully");
+            logger.info("deleted the recording: " + file + "succesfully");
             return true;
         }
         catch (err) {
-            console.log(err);
+            logger.error( {category: 'content folder'}, err);
             return false;
         }
     }
